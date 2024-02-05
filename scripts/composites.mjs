cc838514a9f43fcc3633d2e85aba867d245e8792
Mark Krasner <markkrasner@Marks-MacBook.local>
2024-02-05 12:40:07 -0500

    Fixes for bruno

--- scripts/composites.mjs
+++ scripts/composites.mjs
@@ -21,23 +21,99 @@ const ceramic = new CeramicClient("http://localhost:7007");
 export const writeComposite = async (spinner) => {
   await authenticate();
   
-  const testComposite = await createComposite(
+  const profileComposite = await createComposite(
     ceramic,
-    "./composites/00-test.graphql"
+    "./composites/00-basicProfile.graphql"
   );
 
+  const postsSchema = readFileSync("./composites/01-posts.graphql", {
+    encoding: "utf-8",
+  }).replace("$PROFILE_ID", profileComposite.modelIDs[0]);
 
-  await writeEncodedComposite(testComposite, "./src/__generated__/definition.json");
+  const postsComposite = await Composite.create({
+    ceramic,
+    schema: postsSchema,
+  });
+
+  const propsSchema = readFileSync("./composites/01-proposals.graphql", {
+    encoding: "utf-8",
+  }).replace("$PROFILE_ID", profileComposite.modelIDs[0]);
+
+  const propsComposite = await Composite.create({
+    ceramic,
+    schema: propsSchema,
+  });
+
+  const followingSchema = readFileSync("./composites/02-following.graphql", {
+    encoding: "utf-8",
+  }).replace("$PROFILE_ID", profileComposite.modelIDs[0]);
+
+  const followingComposite = await Composite.create({
+    ceramic,
+    schema: followingSchema,
+  });
+
+  const postProfileSchema = readFileSync(
+    "./composites/03-postsProfile.graphql",
+    {
+      encoding: "utf-8",
+    }
+  )
+    .replace("$POSTS_ID", postsComposite.modelIDs[1])
+    .replace("$PROFILE_ID", profileComposite.modelIDs[0]);
+
+  const postsProfileComposite = await Composite.create({
+    ceramic,
+    schema: postProfileSchema,
+  });
+
+  const commentsSchema = readFileSync("./composites/04-comments.graphql", {
+    encoding: "utf-8",
+  })
+    .replace("$POSTS_ID", postsComposite.modelIDs[1])
+    .replace("$PROFILE_ID", profileComposite.modelIDs[0]);
+
+  const commentsComposite = await Composite.create({
+    ceramic,
+    schema: commentsSchema,
+  });
+
+  const commentsPostsSchema = readFileSync(
+    "./composites/05-commentsPosts.graphql",
+    {
+      encoding: "utf-8",
+    }
+  )
+    .replace("$COMMENTS_ID", commentsComposite.modelIDs[2])
+    .replace("$POSTS_ID", postsComposite.modelIDs[1]);
+
+  const commentsPostsComposite = await Composite.create({
+    ceramic,
+    schema: commentsPostsSchema,
+  });
+
+  const composite = Composite.from([
+    profileComposite,
+    postsComposite,
+    followingComposite,
+    postsProfileComposite,
+    commentsComposite,
+    commentsPostsComposite,
+    propsComposite
+  ]);
+
+
+  await writeEncodedComposite(composite, "./definition.json");
   spinner.info("creating composite for runtime usage");
   await writeEncodedCompositeRuntime(
     ceramic,
-    "./src/__generated__/definition.json",
-    "./src/__generated__/definition.js"
+    "./definition.json",
+    "./definition.js"
   );
   spinner.info("deploying composite");
   const deployComposite = await readEncodedComposite(
     ceramic,
-    "./src/__generated__/definition.json"
+    "./definition.json"
   );
 
   await deployComposite.startIndexingOn(ceramic);
